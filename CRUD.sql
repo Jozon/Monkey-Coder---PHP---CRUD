8b9a5388b22125aba656eca0aef45b833dcc1a00
Xozon <65068490+Jozon@users.noreply.github.com>
2021-10-28 01:29:12 +0600

    Add files via upload

---
+++ CRUD.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 27, 2021 at 09:26 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `student_name` varchar(45) NOT NULL,
+  `phone` varchar(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `reg_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `student_name`, `phone`, `gender`, `city`, `reg_time`) VALUES
+(1, 'Arup Sarker', '012345678956', 'Male', 'Khulna', '2021-10-26 18:02:52'),
+(2, 'Ibrahim Hossain', '0145986332', 'Male', 'Bhola', '2021-10-26 18:14:13'),
+(3, 'Jozon Anwar', '01948159289', 'Male', 'Bhandaria', '2021-10-26 18:14:44'),
+(4, 'Korin Uddin', '15421320563', 'Male', 'Bheramara', '2021-10-27 17:45:52'),
+(5, 'jalal uddin', '55653182162', 'Male', 'Bhairab Bazar', '2021-10-27 17:46:13'),
+(6, 'Jorina Khatun', '63543513358', 'Female', 'Bhandaria', '2021-10-27 17:46:28'),
+(7, 'Jozon Anwar', '01948159289', 'Male', 'Bhandaria', '2021-10-27 17:47:10'),
+(8, 'Ibrahim Hossain', '01948159289', 'Male', 'Bhairab Bazar', '2021-10-27 17:47:38'),
+(9, 'Arup Sarker', '01256985263', 'Male', 'Char Bhadrasan', '2021-10-27 17:47:46'),
+(10, 'Amar Nam', '01948159289', 'Male', 'Bhola', '2021-10-27 17:48:04'),
+(11, 'heelal uddin', '01236454987', 'Male', 'Bogra', '2021-10-27 17:48:49'),
+(12, 'Arup Sarker', '1234554415', 'Male', 'Abhaynagar', '2021-10-27 17:49:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `reg_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `reg_time`) VALUES
+(1, 'jozon', '123456', '2021-10-25 19:25:15'),
+(2, 'jozon', '25d55ad283aa400af464c76d713c07ad', '2021-10-26 05:19:40');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
