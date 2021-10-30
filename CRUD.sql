d25475bafd7c2a60f1a728627678c012f2a117fc
Xozon <65068490+Jozon@users.noreply.github.com>
2021-10-31 01:26:11 +0600

    Add files via upload

--- CRUD.sql
+++ CRUD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2021 at 09:26 PM
+-- Generation Time: Oct 30, 2021 at 09:24 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -41,18 +41,12 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `student_name`, `phone`, `gender`, `city`, `reg_time`) VALUES
-(1, 'Arup Sarker', '012345678956', 'Male', 'Khulna', '2021-10-26 18:02:52'),
-(2, 'Ibrahim Hossain', '0145986332', 'Male', 'Bhola', '2021-10-26 18:14:13'),
-(3, 'Jozon Anwar', '01948159289', 'Male', 'Bhandaria', '2021-10-26 18:14:44'),
-(4, 'Korin Uddin', '15421320563', 'Male', 'Bheramara', '2021-10-27 17:45:52'),
-(5, 'jalal uddin', '55653182162', 'Male', 'Bhairab Bazar', '2021-10-27 17:46:13'),
-(6, 'Jorina Khatun', '63543513358', 'Female', 'Bhandaria', '2021-10-27 17:46:28'),
 (7, 'Jozon Anwar', '01948159289', 'Male', 'Bhandaria', '2021-10-27 17:47:10'),
 (8, 'Ibrahim Hossain', '01948159289', 'Male', 'Bhairab Bazar', '2021-10-27 17:47:38'),
 (9, 'Arup Sarker', '01256985263', 'Male', 'Char Bhadrasan', '2021-10-27 17:47:46'),
 (10, 'Amar Nam', '01948159289', 'Male', 'Bhola', '2021-10-27 17:48:04'),
 (11, 'heelal uddin', '01236454987', 'Male', 'Bogra', '2021-10-27 17:48:49'),
-(12, 'Arup Sarker', '1234554415', 'Male', 'Abhaynagar', '2021-10-27 17:49:00');
+(12, 'Arup Sarker Dada', '1234554415', 'Male', 'Abhaynagar', '2021-10-27 17:49:00');
 
 -- --------------------------------------------------------
 
